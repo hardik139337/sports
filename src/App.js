5fd5c4109e211d2ad02b3e9100623c462698e99e
Yudiz Solutions <yudizsolutions@omega.local>
2021-09-06 13:45:43 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,59 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import { Route } from 'react-router-dom';
 
 import './App.css';
-import Card from './components/card';
-import Header from './components/header'
 import Detail from './components/detail'
-import Home  from './components/home';
+import Home from './components/home';
+import Header from './components/header';
 function App() {
 
-  // const [state, setstate] = useState(5)
-  // useEffect(() => {
-  //   //  const url = 'https://backend.sports.info/api/v1/posts/recent'
-  //   //  const data = { "nStart": 0, "nLimit": 1, "eSort": "Latest", "bRemoveBannerPosts": true }
-  //   //   axios.post(url, data, { headers: { 'Content-Type': 'application/json' } }).then(
-  //   //     (res) => {
-  //   //       setstate(res.data);
-  //   //       console.log(res.data);
-  //   //     })
-
-  //   fetch('https://backend.sports.info/api/v1/posts/recent', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: { "nStart": 0, "nLimit": 1, "eSort": "Latest", "bRemoveBannerPosts": true }
-  //   }).then(
-  //     (res) => { return res.json() }).then(
-  //       (data) => { console.log(data) }
-  //     )
-
-  //   // console.log(data)
-  //   // setstate(data)
-
-  // }, [])
 
   return (
     <div className="App">
 
-
-<Route path='/detail' component={Detail}  exact/>
-<Route path='/' component={Home}  exact/>
-
-
       {/* <Header></Header> */}
-      {/* {Array(state).fill("").map((a) => { return <Card /> })
+      <Route path='/detail' component={Detail} exact />
+      <Route path='/' component={Home} exact />
 
-      }
-    
-    <button className='btn' onClick={() => { setstate(state + 5) }}>loard more</button>
-    
-     */}
+
     </div>
-
-    // <div>
-    //   <Detail></Detail>
-    // </div>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -3,23 +3,16 @@ import React from 'react'
 export default function header() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <img src="../../public/logo_v2.svg" alt="" />
-                <a className="navbar-brand" href="#">Navbar w/ text</a>
-                <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+           
+<div class="header">
+  <a href="#default" class="logo">CompanyLogo</a>
+  <div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+</div>
+
         </div>
     )
 }
