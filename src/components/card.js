64ff7376673af893a77c30f5d0713a75d45a7706
hardik <hardikpatoliya15@gmail.com>
2021-09-09 18:39:43 +0530

    m

--- src/components/card.js
+++ src/components/card.js
@@ -6,16 +6,16 @@ export default function Card(props) {
   return (
     <div>
       <div className="card row">
-        <div className="col-md-4">
+        <div className="col-md-5">
           <img
             src={props.sImage}
             alt="img"
             style={{ width: "100%", height: "auto" }}
           />
         </div>
-        <div className="col-md-8">
+        <div className="col-md-7">
           <h5>{props.sTitle}</h5>
-          <p> {props.sDescription}</p>
+          <p> {props.sDescription.slice(0, 200)}</p>
           <div style={{ display: "flex", justifyContent: "space-between" }}>
             <div>
               <p>Sports Info 6 months ago</p>
